27310eb0b7fe5b25c0ec52ef67245b1c4109a663
fumikito <guy@hametuha.com>
2019-01-24 22:20:49 +0900

    Add demo url input.

--- hooks/admin-product-meta.php
+++ hooks/admin-product-meta.php
@@ -28,44 +28,27 @@ add_action( 'add_meta_boxes', function( $post_type ) {
 		 		$text = <<<'HTML'
 				<p>
 					<label for="%1$s">%2$s</label>
-					<input type="text" name="%1$s" id="%1$s" value="%4$s" placeholder="%3$s" />
+					<input type="text" name="%1$s" id="%1$s" value="%4$s" placeholder="%3$s" class="widefat" />
 				</p>
 HTML;
 				$value = get_post_meta( $post->ID, $key, true );
 				printf( $text, $key, esc_html( $label ), esc_attr( $placeholder ), esc_attr( $value ) );
 			}
 		 }, $post_type, 'side', 'high' );
-		
+		 
+		 // Demo Setting.
 		add_meta_box( 'hakama-product-demo', __( 'Demo Setting', 'hakama' ), function( $post ) {
 			?>
-			<label></label><br />
-			<input type="url" class="regular-text" name="" value="" />
-			
+			<p class="description">
+				<?php esc_html_e( 'Enter demo URL for your theme/plugin.', 'hakama' ) ?>
+			</p>
+			<p class="description">
+				<label for="demo_url">
+					<?php esc_html_e( 'Demo URL', 'hakama' ) ?>
+				</label><br/>
+				<input id="demo_url" name="_demo_url" type="url" class="widefat" value="<?php echo esc_attr( get_post_meta( $post->ID, '_demo_url', true ) ) ?>" />
+			</p>
 			<?php
-			printf( '<p class="description">%s</p>', esc_html__( 'Enter required version. No minor version required. e.g. 4.9', 'hakama' ) );
-			foreach ( [
-				'_required_wp_version'  => [
-					__( 'Required WordPress Version', 'hakama' ),
-					'e.g. 4.9',
-				],
-				'_tested_up_to'         => [
-					__( 'Tested Up To', 'hakama' ),
-					'e.g. 4.9 ',
-				],
-				'_required_php_version' => [
-					__( 'Required PHP Version', 'hakama' ),
-					'e.g. 5.4',
-				],
-			] as $key => list( $label, $placeholder ) ) {
-				$text = <<<'HTML'
-				<p>
-					<label for="%1$s">%2$s</label>
-					<input type="text" name="%1$s" id="%1$s" value="%4$s" placeholder="%3$s" />
-				</p>
-HTML;
-				$value = get_post_meta( $post->ID, $key, true );
-				printf( $text, $key, esc_html( $label ), esc_attr( $placeholder ), esc_attr( $value ) );
-			}
 		}, $post_type, 'side', 'high' );
 	}
 } );
@@ -85,6 +68,7 @@ add_action( 'save_post', function( $post_id, $post ) {
 		'_required_wp_version',
 		'_tested_up_to',
 		'_required_php_version',
+		'_demo_url'
 	] as $key ) {
 		update_post_meta( $post_id, $key, $_REQUEST[ $key ] );
 	}
--- template-parts/content-product.php
+++ template-parts/content-product.php
@@ -3,10 +3,16 @@
 	<?php hakama_template( 'content-product-gallery' ) ?>
 	
 	<div class="row product-main-buttons">
-		<div class="col">
+		<div class="col mb-1">
 			<?php hakama_document_link( get_the_ID(), '', 'btn btn-outline-primary btn-lg btn-block', 'file-signature' ) ?>
 		</div>
-		<div class="col">
+		<div class="col mb-1">
+			<?php $demo = get_post_meta( get_the_ID(), '_demo_url', true ); ?>
+			<a class="btn btn-outline-primary btn-lg btn-block<?php echo $demo ? '' : ' disabled' ?>" href="<?php echo esc_url( $demo ?: '#' ) ?>" target="_blank">
+				<i class="far fa-eye"></i> <?php esc_html_e( 'Demo', 'hakama' ); ?>
+			</a>
+		</div>
+		<div class="col mb-1">
 			<a class="btn btn-outline-primary btn-lg btn-block" href="<?php echo hakama_support_page() ?>">
 				<i class="fa fa-life-ring"></i> <?php esc_html_e( 'Support', 'hakama' ); ?>
 			</a>
