a3eb6e290e8cd84fc2bb80201d0a29bf3d3da6c5
fumikito <guy@hametuha.com>
2019-12-23 18:25:27 +0900

    Update readme for collaboration.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # hakama
+
 WooCommerce theme for kunoichi.
+
+## Installation
+
+```
+git clone git@github.com:kuno1/hakama.git
+cd hakama
+# Install libraries.
+composer install
+# Buils assets.
+npm install && npm start
+# Watch changes.
+npm run watch
+```
+
+## Dependencies
+
+This theme is highly depends on [Makibishi](https://github.com/kuno1/makibishi). Please install and activate it before using this theme.
+
+## Deployment
+
+Same as [Makibishi](https://github.com/kuno1/makibishi).
+
+```
+# Staging
+./bin/deploy.sh staging
+# Production
+./bin/deploy.sh production
+```
---
+++ bin/deploy.sh
@@ -0,0 +1,38 @@
+#! /usr/bin/env bash
+
+set -e
+
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
+echo -e "Deploying Hakama...\n"
+
+# Make templ dir
+cd /tmp
+TMP_DIR=`mktemp -d tmp.XXXXXXXXXX`
+cd $TMP_DIR
+pwd
+
+case $1 in
+	"staging")
+		HOST="kunoichi-staging"
+		git clone git@github.com:kuno1/hakama.git
+		cd hakama
+		bash ./bin/cleanup.sh
+		echo -e "Master branch built";;
+	"production")
+		HOST="kunoichi-production"
+		VERSION=`get_latest_release "kuno1/hakama"`
+		echo -e "Deploying version $VERSION"
+		wget "https://github.com/kuno1/hakama/releases/download/$VERSION/hakama.zip"
+		unzip hakama.zip
+		rm hakama.zip;;
+		
+	*)
+		exit "You must specify 'staging' or 'production'";;
+esac
+
+rsync -rvct --delete . nginx@$HOST:/var/web/wp/wp-content/themes/hakama
