fe62fcdf8c52e7f7c6b8ed4dd3f52ff020a90d63
fumikito <guy@hametuha.com>
2020-02-27 18:43:46 +0900

    Fix product list.

--- template-parts/kbl/post-loop-product.php
+++ template-parts/kbl/post-loop-product.php
@@ -7,21 +7,23 @@ $thumbnail = has_post_thumbnail() ? sprintf( 'background-image: url(\'%s\')', es
 // TODO: change thumbnail size as of product taxonomy.
 ?>
 <li <?php post_class( 'product-list-item' ); ?>>
-	<div class="product-list-thumbnail" style="<?php echo esc_attr( $thumbnail ) ?>">
-		<?php if ( $category = hakama_top_category() ) : ?>
-			<a class="product-list-term" href="<?php echo get_term_link( $category ) ?>">
-				<?php echo esc_html( $category->name ) ?>
-			</a>
-		<?php endif;?>
-	</div>
-	<div class="product-list-body">
-
-		<p class="product-list-title"><?php the_title() ?></p>
-
-		<div class="product-list-meta">
-			<?php the_excerpt(); ?>
+	<div class="product-list-inner">
+		<div class="product-list-thumbnail" style="<?php echo esc_attr( $thumbnail ) ?>">
+			<?php if ( $category = hakama_top_category() ) : ?>
+				<a class="product-list-term" href="<?php echo get_term_link( $category ) ?>">
+					<?php echo esc_html( $category->name ) ?>
+				</a>
+			<?php endif; ?>
 		</div>
+		<div class="product-list-body">
 
+			<p class="product-list-title"><?php the_title() ?></p>
+
+			<div class="product-list-meta">
+				<?php the_excerpt(); ?>
+			</div>
+
+		</div>
 
 		<a href="<?php the_permalink(); ?>" class="product-list-link">
 			<?php echo esc_html_x( 'Detail', 'product-link', 'hakama' ) ?> &raquo;
